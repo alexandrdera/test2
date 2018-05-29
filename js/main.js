d1aca52fae353caa19cef803f5a43865953e488c
Alexandr Derachic <alexandr.dera@gmail.com>
2018-05-29 20:25:04 +0300

    Array json added

--- js/main.js
+++ js/main.js
@@ -1,17 +1,20 @@
 //Входные json данные
-var JSON_data = '{"cost":"35", "title":"Notebook", "description":"MSI Leopard"}';
+var JSON_data = '[{"cost":"35", "title":"Notebook", "description":"MSI Leopard"},{"cost":"25", "title":"Samsung", "description":"R528"},{"cost":"15", "title":"HP", "description":"Povilion"}]';
 
 $("#show_more").on('click', function() {
     var last_item = $("div.items").last(); //получаем последний блок списка товаров
     item = JSON.parse(JSON_data); //читаем объекты из строки в формате JSON
 
-    //Используем шабон new_item для вставки новых товаров
-    var new_item = "<div class='item'><div class='cost'>"+ item.cost + 
-    "</div><div class='title'>"+ item.title +
-    "</div><div class='description'>"+ item.description +
-    "</div></div>"
-
-    last_item.append(new_item); //добавляем новые товары в конец списка
+    item.forEach(element => {
+        
+        //Используем шабон new_item для вставки новых товаров
+        var new_item = "<div class='item'><div class='cost'>"+ element.cost + 
+        "</div><div class='title'>"+ element.title +
+        "</div><div class='description'>"+ element.description +
+        "</div></div><hr>"
+    
+        last_item.append(new_item); //добавляем новые товары в конец списка
+    });
 });
 
   
